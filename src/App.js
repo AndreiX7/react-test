1b24cb2ab35fc399424e5ec50e9d4bf1c877e6f8
andrei.albiller <andrei@ubx.ph>
2023-02-06 18:28:55 +0800

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: 'Old'
+  }
+
+  handleClick = () => {
+    this.setState({
+      name: 'New'
+    })
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center', fontSize: 64 }}>
+        {this.state.name}
+        <br />
+        <button onClick={this.handleClick}>
+          THIS IS A BUTTON
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
