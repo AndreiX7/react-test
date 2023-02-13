57bb3680a10ab0a85e4494db0a5f9779562400bd
ands-mbp <andrei@ubx.ph>
2023-02-13 09:44:09 +0800

    hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-class App extends Component {
-  state = {
-    name: 'Old'
-  }
+function App(props) {
+  const [objData, setObjData] = useState([{ data: "One" }, { data: "Three" }]);
+  const [anotherObjData, setAnotherObjData] = useState([{ name: "Andi", age: 26 }, { name: "Ands", age: 27 }]);
+  const [isValidated, setIsValidated] = useState(true)
+  const [staticString, setStaticString] = useState("This is a string");
 
-  handleClick = () => {
-    this.setState({
-      name: 'New'
-    })
-  }
+  useEffect(() => {
+    const oldData = [...objData]
+    const anotherOldData = [...anotherObjData]
+    oldData[1] = { data: "Two" }
+    anotherOldData[0].name = "Andrei"
+    anotherOldData[0].age = 28
+    setObjData(oldData)
+    setAnotherObjData(anotherOldData)
+    setIsValidated(true)
+    setStaticString("This is an updated string")
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // execute once
 
-  render() {
-    return (
-      <div style={{ textAlign: 'center', fontSize: 64 }}>
-        {this.state.name}
-        <br />
-        <button onClick={this.handleClick}>
-          THIS IS A BUTTON
-        </button>
+  if (isValidated) {
+    return ( 
+      <div style={{ fontSize: 64 }}>
+        <p>{objData[1]?.data}</p>
+        <p>{anotherObjData[0]?.name}</p>
+        <p>{anotherObjData[0]?.age}</p>
+        <p>{staticString}</p>
       </div>
-    );
+    )
   }
+  
 }
 
 export default App;
